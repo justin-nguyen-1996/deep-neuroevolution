8af80d7744ac5e3591d3e25edccb5dffb16c518c
Justin Nguyen <2014justinnguyen@gmail.com>
2019-12-01 08:36:59 -0600

    Some more ms pacman data

--- quick_viz.sh
+++ quick_viz.sh
@@ -2,6 +2,6 @@
 # quick_viz.sh
 
 ENV='MsPacmanNoFrameskip-v4'
-HDF5_FILE='Experiments/NSR_ES_MsPacman/snapshot_iter00015_rew515.h5'
+HDF5_FILE='Experiments/NSR_ES_MsPacman/snapshot_iter00060_rew904.h5'
 
 python -m scripts.viz $ENV $HDF5_FILE
