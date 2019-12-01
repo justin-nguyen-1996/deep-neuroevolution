c5012819a9a7b757d164c061180872e9127ce7ad
Justin Nguyen <jnguyen@login1.maverick2.tacc.utexas.edu>
2019-12-01 08:39:00 -0600

    Some more ms pacman data

--- run_all.sh
+++ run_all.sh
@@ -19,10 +19,10 @@ tmux send-keys -t :0.0 'redis-server redis_config/redis_master.conf' C-m
 tmux send-keys -t :0.1 'redis-server redis_config/redis_local_mirror.conf' C-m
 # Send commands to second window's pane 0 (master setup for ES)
 tmux send-keys -t :1.0 '. scripts/local_env_setup.sh' C-m
-tmux send-keys -t :1.0 'python -m es_distributed.main master --master_socket_path /tmp/es_redis_master.sock --algo '$ALGO' --exp_file '"$EXP_FILE"' &' C-m
+tmux send-keys -t :1.0 'python -m es_distributed.main master --master_socket_path /tmp/es_redis_master.sock --algo '$ALGO' --exp_file '"$EXP_FILE" C-m
 # Send commands to second window's pane 1 (worker setup for ES)
 tmux send-keys -t :1.1 '. scripts/local_env_setup.sh' C-m
 tmux send-keys -t :1.1 'sleep 30' C-m
-tmux send-keys -t :1.1 'python -m es_distributed.main workers --master_host localhost --relay_socket_path /tmp/es_redis_relay.sock --algo '$ALGO' --num_workers 40 > /dev/null &' C-m
+tmux send-keys -t :1.1 'python -m es_distributed.main workers --master_host localhost --relay_socket_path /tmp/es_redis_relay.sock --algo '$ALGO' --num_workers 40' C-m
 # Attach to tmux session
 #tmux attach # NOTE: don't attach because TACC servers don't have tty terminals
