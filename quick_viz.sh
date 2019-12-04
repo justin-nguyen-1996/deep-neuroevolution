bed8a5fd9480e604262bf110a9fef97fb25d3eeb
Justin Nguyen <2014justinnguyen@gmail.com>
2019-12-04 00:28:06 -0600

    Moved snapshots to correct folder

--- quick_viz.sh
+++ quick_viz.sh
@@ -1,7 +1,23 @@
 
 # quick_viz.sh
 
-ENV='MsPacmanNoFrameskip-v4'
-HDF5_FILE='Experiments/NSR_ES_MsPacman/snapshot_iter00060_rew904.h5'
+# Source venv
+. env_activate.sh
 
-python -m scripts.viz $ENV $HDF5_FILE
+# MsPacman
+#ENV='MsPacmanNoFrameskip-v4'
+#HDF5_FILE='Experiments/NSR_ES_MsPacman/snapshot_iter00060_rew904.h5'
+
+# Frostbite ES
+#ENV='FrostbiteNoFrameskip-v4'
+#HDF5_FILE='Experiments/ES_Frostbite/snapshot_iter00020_rew1006.h5'
+
+# Frostbite normal ram state BC
+#ENV='FrostbiteNoFrameskip-v4'
+#HDF5_FILE='Experiments/NSR_ES_Frostbite/snapshot_iter00040_rew1271.h5'
+
+# Frostbite BC stepping on ice
+ENV='FrostbiteNoFrameskip-v4'
+HDF5_FILE='Experiments/NSR_ES_Frostbite_BC_StepOnIce/snapshot_iter00035_rew2921.h5'
+
+python3 -m scripts.viz $ENV $HDF5_FILE
