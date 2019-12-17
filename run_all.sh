9ba6b71ec7ea6405bba9850d4e87d586e853ef53
Justin Nguyen <2014justinnguyen@gmail.com>
2019-12-17 09:23:28 -0600

    Final version of run all script (updated to include log file argument)

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if [ $# != 2 ]; then
-    echo 'Needs two arguments (algo and config file)'
+if [ $# != 3 ]; then
+    echo 'Needs three arguments (algo, config file, and log dir)'
     exit
 fi
 
 # Set variables for the arguments
 ALGO=$1
 EXP_FILE=$2
+LOG_DIR=$3
 # Create tmux session in detached mode
 tmux new -d
 # Create splits in both windows
@@ -19,10 +20,10 @@ tmux send-keys -t :0.0 'redis-server redis_config/redis_master.conf' C-m
 tmux send-keys -t :0.1 'redis-server redis_config/redis_local_mirror.conf' C-m
 # Send commands to second window's pane 0 (master setup for ES)
 tmux send-keys -t :1.0 '. scripts/local_env_setup.sh' C-m
-tmux send-keys -t :1.0 'python -m es_distributed.main master --master_socket_path /tmp/es_redis_master.sock --algo '$ALGO' --exp_file '"$EXP_FILE" C-m
+tmux send-keys -t :1.0 'python -m es_distributed.main master --log_dir '$LOG_DIR' --master_socket_path /tmp/es_redis_master.sock --algo '$ALGO' --exp_file '"$EXP_FILE" C-m
 # Send commands to second window's pane 1 (worker setup for ES)
 tmux send-keys -t :1.1 '. scripts/local_env_setup.sh' C-m
 tmux send-keys -t :1.1 'sleep 30' C-m
-tmux send-keys -t :1.1 'python -m es_distributed.main workers --master_host localhost --relay_socket_path /tmp/es_redis_relay.sock --algo '$ALGO' --num_workers 40' C-m
+tmux send-keys -t :1.1 'python -m es_distributed.main workers --master_host localhost --relay_socket_path /tmp/es_redis_relay.sock --algo '$ALGO' --num_workers 60' C-m
 # Attach to tmux session
 #tmux attach # NOTE: don't attach because TACC servers don't have tty terminals
